d41df8917f5fbd848cdb33c72b1c2aecc4e61e3c
CreWeb <58531299+IlyaPicasso@users.noreply.github.com>
2019-12-05 03:11:18 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// window.onscroll = function () {
+//     scrollFunction()
+// };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.getElementById("navbar").style.padding = "20px 0";
+//         document.getElementById("logo").style.fontSize = "1.5em";
+//     } else {
+//         document.getElementById("navbar").style.padding = "25px 0";
+//         document.getElementById("logo").style.fontSize = "2.5em"
+//     }
+// }
